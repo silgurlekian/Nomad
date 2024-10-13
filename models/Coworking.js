a11b41f9ff099791f59b777f257f4c91db0bcbe9
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-10-13 11:13:33 -0300

    Crear modelos de Usuario y Coworking

---
+++ models/Coworking.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const coworkingSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    direccion: { type: String, required: true },
+    ciudad: { type: String, required: true },
+    telefono: { type: String },
+    email: { type: String },
+    website: { type: String },
+    descripcion: { type: String },
+    servicios: [{ type: String }],
+}, { timestamps: true });
+
+const Coworking = mongoose.model('Coworking', coworkingSchema);
+
+export default Coworking;
