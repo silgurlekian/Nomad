ec64a662f77b1c4424133e65cac4ad7791a227ea
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-10-12 21:51:38 -0300

    Agregar validaciones

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { check } from 'express-validator';
+import { validateRequest } from '../middlewares/validateRequest.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'Por favor, ingresa un email válido').isEmail(),
+    check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 })
+], validateRequest, registerUser);
+
+router.post('/login', [
+    check('email', 'Por favor, ingresa un email válido').isEmail(),
+    check('password', 'La contraseña es obligatoria').exists()
+], validateRequest, loginUser);
 
 export default router;
