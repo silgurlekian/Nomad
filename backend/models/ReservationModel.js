14ff7eb486db741e1d9847973cb5396907c710d2
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-11 16:20:11 -0300

    fix model de reservas

--- backend/models/ReservationModel.js
+++ backend/models/ReservationModel.js
@@ -1,39 +1,45 @@
-const reservationSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+import mongoose from "mongoose";
+
+const reservationSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    additionalNotes: {
+      type: String,
+    },
+    numberOfPlaces: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Relación con el modelo de usuario
+      required: true,
+    },
+    spaceId: {
+      // Nuevo campo para el ID del espacio reservado
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Space", // Relación con el modelo de espacio
+      required: true,
+    },
   },
-  date: {
-    type: String,
-    required: true,
-  },
-  startTime: {
-    type: String,
-    required: true,
-  },
-  endTime: {
-    type: String,
-    required: true,
-  },
-  additionalNotes: {
-    type: String,
-  },
-  numberOfPlaces: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Relación con el modelo de usuario
-    required: true,
-  },
-  spaceId: {  // Nuevo campo para el ID del espacio reservado
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Space", // Relación con el modelo de espacio
-    required: true,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Reservation = mongoose.model("Reservation", reservationSchema);
 
