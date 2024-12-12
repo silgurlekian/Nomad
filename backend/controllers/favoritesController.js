056aaa75974f4726d577ff03c37eeee90e14e328
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-12 10:31:50 -0300

    eliminar favorito

--- backend/controllers/favoritesController.js
+++ backend/controllers/favoritesController.js
@@ -22,3 +22,26 @@ export const getFavorites = async (req, res) => {
     res.status(500).json({ message: "Error al obtener favoritos", error });
   }
 };
+
+export const removeFavorite = async (req, res) => {
+    const userId = req.user.id;
+    const favoriteId = req.params.favoriteId;
+  
+    try {
+      const favorite = await Favorite.findById(favoriteId);
+  
+      if (!favorite) {
+        return res.status(404).json({ message: "Favorito no encontrado" });
+      }
+  
+      if (favorite.userId.toString() !== userId) {
+        return res.status(403).json({ message: "No autorizado a eliminar este favorito" });
+      }
+  
+      await favorite.remove();
+      res.status(200).json({ message: "Favorito eliminado correctamente" });
+    } catch (error) {
+      res.status(500).json({ message: "Error al eliminar el favorito", error });
+    }
+  };
+  
--- backend/routes/favoritesRoutes.js
+++ backend/routes/favoritesRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/", protect, addFavorite);
 router.get("/", protect, getFavorites);
+router.delete("/:favoriteId", protect, removeFavorite);
 
 export default router;
