7f51d5c866c6f83e0302adcc2a6401d793c5db95
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-12 10:18:50 -0300

    Fix FavoriteModel

--- backend/models/FavoriteModel.js
+++ backend/models/FavoriteModel.js
@@ -4,3 +4,7 @@ const FavoriteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   espacioId: { type: mongoose.Schema.Types.ObjectId, ref: "Space", required: true },
 });
+
+const Favorite = mongoose.model("Favorite", FavoriteSchema);
+
+export default Favorite;
