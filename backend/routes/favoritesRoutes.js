57417a1078a5c02003394026ab93e1f146e28457
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-12 10:40:35 -0300

    fix favoritesRoutes para remove

--- backend/routes/favoritesRoutes.js
+++ backend/routes/favoritesRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addFavorite, getFavorites } from "../controllers/favoritesController.js";
+import { addFavorite, getFavorites, removeFavorite } from "../controllers/favoritesController.js"; 
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, addFavorite);
 router.get("/", protect, getFavorites);
-router.delete("/:favoriteId", protect, removeFavorite);
+router.delete("/:favoriteId", protect, removeFavorite);  
 
 export default router;
