cf0c6f7ea71b5ee5481324fdced03bc344ca7218
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-18 14:34:27 -0300

    Agregar buscador a listado de espacios

--- frontend/src/components/SpacesList.js
+++ frontend/src/components/SpacesList.js
@@ -5,11 +5,15 @@ import Loading from "../components/Loading";
 
 const SpacesList = () => {
   const [spaces, setSpaces] = useState([]);
+  const [filteredSpaces, setFilteredSpaces] = useState([]);
   const [error, setError] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [showModal, setShowModal] = useState(false); // Estado para mostrar el modal
+  const [showModal, setShowModal] = useState(false);
   const [selectedSpace, setSelectedSpace] = useState(null);
-  const [cargando, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [spaceTypes, setSpaceTypes] = useState([]);
 
   const navigate = useNavigate();
 
@@ -31,13 +35,16 @@ const SpacesList = () => {
         setIsAdmin(true);
 
         const config = { headers: { Authorization: `Bearer ${token}` } };
-        const response = await axios.get(
-          "https://nomad-znm2.onrender.com/api/spaces?page=1&limit=5",
-          config
-        );
-        setSpaces(response.data);
+        const [spacesResponse, typesResponse] = await Promise.all([
+          axios.get("https://nomad-znm2.onrender.com/api/spaces", config),
+          axios.get("https://nomad-znm2.onrender.com/api/spacesType", config),
+        ]);
+
+        setSpaces(spacesResponse.data);
+        setFilteredSpaces(spacesResponse.data);
+        setSpaceTypes(typesResponse.data);
       } catch (error) {
-        setError("Error al obtener los espacios: " + error.message);
+        setError("Error al obtener los datos: " + error.message);
       } finally {
         setLoading(false);
       }
@@ -46,19 +53,48 @@ const SpacesList = () => {
     fetchSpaces();
   }, []);
 
+  useEffect(() => {
+    const normalizeString = (str) =>
+      str
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/\s+/g, " ")
+        .trim()
+        .toLowerCase();
+
+    let filtered = spaces.filter((space) =>
+      Object.values(space).some((value) =>
+        value
+          ? normalizeString(value.toString()).includes(
+              normalizeString(searchTerm)
+            )
+          : false
+      )
+    );
+
+    if (selectedTypes.length > 0) {
+      filtered = filtered.filter(
+        (space) =>
+          space.spacesType &&
+          space.spacesType.some((type) => selectedTypes.includes(type.name))
+      );
+    }
+
+    setFilteredSpaces(filtered);
+  }, [searchTerm, selectedTypes, spaces]);
+
   const handleSpaceDeleted = (spaceId) => {
-    setSpaces(spaces.filter((space) => space._id !== spaceId));
-    setShowModal(false); // Cerrar el modal después de eliminar
+    setSpaces((prev) => prev.filter((space) => space._id !== spaceId));
+    setFilteredSpaces((prev) => prev.filter((space) => space._id !== spaceId));
+    setShowModal(false);
   };
 
   const confirmDelete = (space) => {
     setSelectedSpace(space);
-    setShowModal(true); // Mostrar el modal
+    setShowModal(true);
   };
 
   const deleteSpace = async () => {
-    if (!selectedSpace) return;
-
     try {
       const token = localStorage.getItem("token");
       const config = { headers: { Authorization: `Bearer ${token}` } };
@@ -74,7 +110,16 @@ const SpacesList = () => {
     }
   };
 
-  if (cargando) return <Loading />;
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    setSelectedTypes((prev) =>
+      prev.includes(value)
+        ? prev.filter((type) => type !== value)
+        : [...prev, value]
+    );
+  };
+
+  if (loading) return <Loading />;
 
   return (
     <div className="container bkg-container mt-4">
@@ -90,100 +135,119 @@ const SpacesList = () => {
           </button>
         )}
       </div>
-      <table className="table table-striped mt-4">
-        <thead>
-          <tr>
-            <th>Imagen</th>
-            <th>Nombre</th>
-            <th>Dirección</th>
-            <th>Ciudad</th>
-            <th>Precio</th>
-            <th>Servicios</th>
-            <th>Tipos de Espacio</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {spaces.map((space) => (
-            <tr key={space._id}>
-              <td>
-                {space.imagen && (
-                  <img
-                    src={`${space.imagen}`}
-                    loading="lazy"
-                    alt={space.nombre}
-                    className="space-image"
-                  />
-                )}
-              </td>
-              <td className="align-middle">{space.nombre}</td>
-              <td className="align-middle">{space.direccion}</td>
-              <td className="align-middle">{space.ciudad}</td>
-              <td className="align-middle" style={{ whiteSpace: "nowrap" }}>
-                $ {space.precio}
-              </td>
-              <td className="align-middle">
-                {space.servicios && Array.isArray(space.servicios)
-                  ? space.servicios.map((service) => service.name).join(", ")
-                  : "Sin servicios"}
-              </td>
-              <td className="align-middle">
-                {space.spacesType && Array.isArray(space.spacesType) ? (
-                  space.spacesType.map((type) => {
-                    const colorMap = {
-                      Coworking: "bg-primary",
-                      "Oficina privada": "bg-success",
-                      Hotel: "bg-success",
-                      Hostel: "bg-info",
-                      Cafetería: "bg-warning",
-                      "Espacio para eventos": "bg-danger",
-                      Aula: "bg-secondary",
-                    };
 
-                    const badgeClass =
-                      colorMap[type.name] || "bg-light text-dark";
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Buscar espacios..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
 
-                    return (
-                      <span
-                        key={type._id}
-                        className={`badge ${badgeClass} me-1`}
-                      >
-                        {type.name}
-                      </span>
-                    );
-                  })
-                ) : (
-                  <span className="badge bg-light text-dark">Sin tipos</span>
-                )}
-              </td>
-              <td className="align-middle">
-                <div className="d-flex align-items-center">
-                  <button
-                    className="btn btn-secondary me-2"
-                    onClick={() => navigate(`/editSpace/${space._id}`)}
-                  >
-                    Editar
-                  </button>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => confirmDelete(space)}
-                  >
-                    Eliminar
-                  </button>
-                </div>
-              </td>
+      <div className="mb-3">
+        {spaceTypes.map((type) => (
+          <div key={type._id} className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value={type.name}
+              id={`checkbox-${type._id}`}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              className="form-check-label"
+              htmlFor={`checkbox-${type._id}`}
+            >
+              {type.name}
+            </label>
+          </div>
+        ))}
+      </div>
+
+      {filteredSpaces.length > 0 ? (
+        <table className="table table-striped mt-4">
+          <thead>
+            <tr>
+              <th>Imagen</th>
+              <th>Nombre</th>
+              <th>Dirección</th>
+              <th>Ciudad</th>
+              <th>Precio</th>
+              <th>Servicios</th>
+              <th>Tipos de Espacio</th>
+              <th>Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredSpaces.map((space) => (
+              <tr key={space._id}>
+                <td className="align-middle">
+                  {space.imagen && (
+                    <img
+                      src={`${space.imagen}`}
+                      loading="lazy"
+                      alt={space.nombre}
+                      className="space-image"
+                    />
+                  )}
+                </td>
+                <td className="align-middle">{space.nombre}</td>
+                <td className="align-middle">{space.direccion}</td>
+                <td className="align-middle">{space.ciudad}</td>
+                <td className="align-middle">${space.precio}</td>
+                <td className="align-middle">
+                  {space.servicios?.map((service) => service.name).join(", ") ||
+                    "Sin servicios"}
+                </td>
+                <td className="align-middle">
+                  {space.spacesType?.map((type) => (
+                    <span
+                      key={type._id}
+                      className={`badge ${
+                        {
+                          Coworking: "bg-primary",
+                          Cafetería: "bg-warning",
+                          Hotel: "bg-success",
+                        }[type.name] || "bg-light text-dark"
+                      } me-1`}
+                    >
+                      {type.name}
+                    </span>
+                  )) || (
+                    <span className="badge bg-light text-dark">Sin tipos</span>
+                  )}
+                </td>
+                <td className="align-middle">
+                  <div className="d-flex align-items-center">
+                    <button
+                      className="btn btn-secondary me-2"
+                      onClick={() => navigate(`/editSpace/${space._id}`)}
+                    >
+                      Editar
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => confirmDelete(space)}
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="alert alert-info mt-4" role="alert">
+          No se encontraron resultados para los criterios de búsqueda
+          seleccionados.
+        </div>
+      )}
 
-      {/* Modal para confirmar eliminación */}
       {showModal && (
-        <div
-          className="modal fade show"
-          tabIndex="-1"
-          style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-        >
+        <div className="modal fade show" style={{ display: "block" }}>
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
@@ -204,17 +268,12 @@ const SpacesList = () => {
               </div>
               <div className="modal-footer">
                 <button
-                  type="button"
                   className="btn btn-secondary"
                   onClick={() => setShowModal(false)}
                 >
                   Cancelar
                 </button>
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  onClick={deleteSpace}
-                >
+                <button className="btn btn-danger" onClick={deleteSpace}>
                   Eliminar
                 </button>
               </div>
